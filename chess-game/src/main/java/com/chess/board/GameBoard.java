990ebe4853f3f17ab7d84abd0b949b56d8d12247
Arthurdlg <114220198+Arthurdlg@users.noreply.github.com>
2024-07-04 12:20:53 +0200

    Board Class v1

--- chess-game/src/main/java/com/chess/board/GameBoard.java
+++ chess-game/src/main/java/com/chess/board/GameBoard.java
@@ -1,4 +1,23 @@
 package main.java.com.chess.board;
 
 public class GameBoard {
+    private int boardId;
+    private Cell[][] board;
+
+    public GameBoard(int boardId, Cell[][] board) {
+        this.boardId = boardId;
+        this.board = board;
+    }
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public Cell[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(Cell[][] board) {
+        this.board = board;
+    }
 }
